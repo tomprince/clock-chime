4a0a7307bda032a0a1b61be513841a063d134acf
Tom Prince <tom.prince@twistedmatrix.com>
2017-01-18 22:31:55 -0700

    Support quater hours.

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,14 @@ fn main() {
     let hour = now + chrono::Duration::minutes(30);
     let hour = hour.with_minute(0).unwrap();
 
-    let file = format!("grandfather-clock-chime-{}", hour.format("%I"));
+    let file = match now.minute() {
+        0...7 | 53...60 => format!("grandfather-clock-chime-{}", hour.format("%I")),
+        8...22 => "grandfather-clock-chime-one-quarter".into(),
+        23...37 => "grandfather-clock-chime-one-half".into(),
+        38...52 => "grandfather-clock-chime-three-quarter".into(),
+        _ => panic!{"Minute outside range."},
+    };
+
     let path = DATA.join(PathBuf::from(file).with_extension("wav"));
     play_file(path);
 }
